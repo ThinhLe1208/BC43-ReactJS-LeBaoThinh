6aba1438f266e28a01bc57485a35ba8b875bb085
ThinhLe1208 <lebaothinh1208@gmail.com>
2023-04-11 20:38:36 +0700

    update #1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import Banner from './Banner';
 import Item from './Item';
+import { BiCollection, BiCloudDownload, BiCode } from 'react-icons/bi';
+import { BsCardHeading, BsBootstrap, BsPatchCheck } from 'react-icons/bs';
+
+const data = [
+    { Icon: BiCollection, title: 'Fresh new layout', content: 'With Bootstrap 5, we\'ve created a fresh new layout for this template!' },
+    { Icon: BiCloudDownload, title: 'Free to download', content: 'As always, Start Bootstrap has a powerful collectin of free templates.' },
+    { Icon: BsCardHeading, title: 'Jumbotron hero header', content: 'The heroic part of this template is the jumbotron hero header!' },
+    { Icon: BsBootstrap, title: 'Feature boxes', content: 'We \'ve created some custom feature boxes using Bootstrap Icons!' },
+    { Icon: BiCode, title: 'Simple clean code', content: 'We keep our dependencies up to date and squash bugs as they come!' },
+    { Icon: BsPatchCheck, title: 'A name you trust', content: 'Start Bootstrap has been the leader in free Bootstrap templates since 2013!' },
+];
 
 export default class Body extends Component {
+    renderItems = () => {
+        return data.map((item, index) => {
+            return <Item key={index} data={item} />;
+        });
+    };
+
     render() {
         return (
             <div>
 
                 <Banner />
 
-                <section class="pt-4">
-                    <div class="container px-lg-5">
-                        <div class="row gx-lg-5">
-                            <Item />
-                            <Item />
-                            <Item />
-                            <Item />
-                            <Item />
-                            <Item />
+                <section className="pt-4">
+                    <div className="container px-lg-5">
+                        <div className="row gx-lg-5">
+                            {this.renderItems()}
                         </div>
                     </div>
-                </section>
+                </section >
 
-            </div>
+            </div >
         );
     }
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { IconContext } from "react-icons";
 
 export default class Item extends Component {
     render() {
+        const { Icon, title, content } = this.props.data;
+
         return (
             <div className="col-lg-6 col-xxl-4 mb-5">
                 <div className="card bg-light border-0 h-100">
                     <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                        <div className="feature d-inline-block bg-primary bg-gradient text-white rounded-3 p-3 " style={{ transform: 'translateY(-50%)' }}><i className="fa-regular fa-star"></i></div>
-                        <h2 className="fs-4 fw-bold">Fresh new layout</h2>
-                        <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                        <div className="feature d-inline-block bg-primary bg-gradient text-white rounded-3 px-3 py-3" style={{ transform: 'translateY(-50%)' }}>
+                            <IconContext.Provider value={{ size: "2em" }}>
+                                <div>
+                                    <Icon />
+                                </div>
+                            </IconContext.Provider>
+                        </div>
+                        <h2 className="fs-4 fw-bold">{title}</h2>
+                        <p className="mb-0">{content}</p>
                     </div>
                 </div>
             </div >
